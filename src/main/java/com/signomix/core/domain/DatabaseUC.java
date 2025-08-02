37b85fceb902d7f2b0c5d0da785051052fa08660
Grzegorz Skorupa <g.skorupa@gmail.com>
2025-08-02 11:21:08 +0200

    Bump version to 2.25.0; add error handling for GroupReport saving in DatabaseUC

--- src/main/java/com/signomix/core/domain/DatabaseUC.java
+++ src/main/java/com/signomix/core/domain/DatabaseUC.java
@@ -971,6 +971,13 @@ public class DatabaseUC {
             e.printStackTrace();
             LOG.warn("Error saving report " + className + ": " + e.getMessage());
         }
+        className = "com.signomix.reports.pre.GroupReport";
+        try {
+            reportDao.saveReport(className, 0L, null, null, null);
+        } catch (IotDatabaseException e) {
+            e.printStackTrace();
+            LOG.warn("Error saving report " + className + ": " + e.getMessage());
+        }
     }
 
 }
